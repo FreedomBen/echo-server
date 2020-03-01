a4d1149ed5e908ba2c235dab0104f541812541c7
Benjamin Porter <FreedomBen@users.noreply.github.com>
2020-03-01 17:48:00 -0500

    Add different formatting for path

--- app.rb
+++ app.rb
@@ -5,8 +5,23 @@ require 'sinatra'
 set :port, 8080
 set :bind, '0.0.0.0'
 
+# request.url         # => 'http://example.com/hello-world?foo=bar'
+def response_with_full_url(request)
+  "#{request.ip} #{request.request_method} #{request.url} - : #{request.params}\n"
+end
+
+# request.fullpath    # => '/hello-world?foo=bar'
+def response_with_full_path(request)
+  "#{request.ip} #{request.request_method} #{request.fullpath} - : #{request.params}\n"
+end
+
+# request.path_info   # => '/hello-world'
+def response_with_path_info(request)
+  "#{request.ip} #{request.request_method} #{request.path_info} - : #{request.params}\n"
+end
+
 %w[get head post put delete options patch].each do |method|
   send method, '/*' do
-    "#{request.ip} #{request.request_method} #{request.url} - : #{request.params}\n"
+    response_with_path_info(request)
   end
 end
